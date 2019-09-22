7c3cc633eaeb10ec67153c4a98e8aeace94acaf0
Apoorv Sarang <44388565+WizardOfGryfindor@users.noreply.github.com>
2019-09-21 17:12:20 -0700

    Another algorithm

--- genDistancePic.py
+++ genDistancePic.py
@@ -1,6 +1,6 @@
 from PIL import Image
 from resizeimage import resizeimage
-img = Image.open('sample.jpg', 'r')
+img = Image.open('A/sample.jpg', 'r')
 img.show()
 img_w, img_h = img.size
 #img = img.resize((20,20))
@@ -10,4 +10,4 @@ bg_w, bg_h = background.size
 offset = (50, 50)
 background.paste(img, offset)
 background.show()
-#background.save('sample_dist.png')
+background.save('sample_dist.png')
