4b829e3144b83a9cc093d6a646aa46c2ebb3011f
Keon Min <keonwoong.min@sjsu.edu>
2019-09-21 19:45:33 -0700

    Update genDistancePic.py

--- genDistancePic.py
+++ genDistancePic.py
@@ -12,17 +12,20 @@ import cv2
 import numpy as np
 import os
 
-img_path = "/Users/keonmin/Documents/Python/Face-Recognition-master/pictures/*.jpg"
-img_names = glob(img_path)
+CONST_HEIGHT = 50;
+CONST_WIDTH = 50;
+CONST_PATH = "/Users/keonmin/Documents/Python/Face-Recognition-master/pictures/*.jpg"
+
+img_names = glob(CONST_PATH)
 for fn in img_names:
     img = cv2.imread(fn, 0)
     dimensions = img.shape
     height = img.shape[0]
     width = img.shape[1]
-    print('Image Height       : ',height)
-    print('Image Width        : ',width)
+    #print('Image Height       : ',height)
+    #print('Image Width        : ',width)
     blank_image = 48 * np.ones((height,width), np.uint8)
-    img=cv2.resize(img,(50,50))
+    img=cv2.resize(img,(CONST_HEIGHT, CONST_WIDTH))
     x_offset=y_offset=50
     blank_image[y_offset:y_offset+img.shape[0], x_offset:x_offset+img.shape[1]] = img
     fn = fn + '_dist.jpg'
