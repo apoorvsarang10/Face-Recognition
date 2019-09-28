cc22fdbf51f08981015f3b11f8106ad815439787
Keon Min <keonwoong.min@sjsu.edu>
2019-09-28 10:07:55 -0700

    removed semicolon

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -12,13 +12,13 @@ import cv2
 import numpy as np
 import os 
 
-CONST_WIDTH = 640;
-CONST_HEIGHT = 480;
+CONST_WIDTH = 640
+CONST_HEIGHT = 480
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('trainer/trainer.yml')
 cascadePath = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascadePath);
+faceCascade = cv2.CascadeClassifier(cascadePath)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
