486e2109be2bbecfdadafc570ecb68ad762ff36c
Keon Min <keonwoong.min@sjsu.edu>
2019-09-28 10:15:04 -0700

    Update FaceRecognition.py

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -37,8 +37,8 @@ cam.set(4, CONST_HEIGHT) # set video height
 minW = 0.1*cam.get(3)
 minH = 0.1*cam.get(4)
 
-#while(True):
-while(cam.isOpened()):
+while(True):
+#while(cam.isOpened()):
     ret, img =cam.read()
     img = cv2.flip(img, -1) # Flip vertically
 
